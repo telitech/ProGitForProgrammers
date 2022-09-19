d30180ce711730c9de3b37ea833aa2fa6be504a5
telitech <telitech@outlook.com>
2022-09-19 12:40:57 +0800

    Add informative lines

--- ProGitForProgrammers/HelloWorld/Program.cs
+++ ProGitForProgrammers/HelloWorld/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ProGitForProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I just added this in Visual Studio");
+        }
+    }
+}
